30391089e8ad9ebffb404e2326a259658ac26594
Lukas Ruegner <ruegnerlukas@gmail.com>
2018-07-29 14:24:14 +0200

    Rename version to 0.7.1

--- src/com/ruegnerlukas/wtupdater/UpdateTXTGenerator.java
+++ src/com/ruegnerlukas/wtupdater/UpdateTXTGenerator.java
@@ -25,7 +25,7 @@ public class UpdateTXTGenerator {
 	
 	
 	public static void main(String[] args) {
-		generateUpdateTxt("New version available (0.7).", null, "0.7");
+		generateUpdateTxt("New version available (0.7.1).", null, "0.7.1");
 	}
 	
 	
